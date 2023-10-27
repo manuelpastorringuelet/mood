c91ea90ba274f22156b1ef31b16cf5e637dcd0a6
Manuel Pastor <pastorringuelet@gmail.com>
2023-10-27 13:26:46 +0200

    fr: added analysis-user relationship in Prisma

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,14 @@
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getData = async () => {
+  const user = await getUserByClerkId()
+  const analysis = await prisma.analysis.findMany({
+    where: {
+      
+    },
+  })
+}
 const History = () => {
   return <div>History</div>
 }
